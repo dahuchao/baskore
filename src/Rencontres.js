2fd3d961b8f44e2e71759d70301c590430251c7d
dahuchao <dahuchao.github@laposte.net>
2017-01-08 17:33:19 +0100

    DAO Rencontres

--- src/Rencontres.js
+++ src/Rencontres.js
@@ -1,16 +1,146 @@
-import { RxMongo, RxCollection } from 'rxmongo'
-import { MongoClient } from 'mongodb'
+var Rx = require("rxjs")
+var {RxMongo, RxCollection} = require('rxmongo')
+var {MongoClient} = require('mongodb')
 
-export class Rencontres {
-  constructor(url) {
-    RxMongo.connect(url).subscribe(db => {
-    }, err => {
-      console.log(`Err: ${err}`)
-    })
-  }
-  lecture(id) {
-    return new RxCollection("rencontres")
-      .find({})
-      .first()
+var rencontre$ = Rx
+  .Observable
+  .from([
+    {
+      id: 1,
+      periode: 1,
+      hote: {
+        nom: "NEC",
+        marque: 11
+      },
+      visiteur: {
+        nom: "USJA",
+        marque: 11
+      }
+    }, {
+      id: 2,
+      date: new Date("2016-09-02"),
+      periode: 1,
+      hote: {
+        nom: "NEC",
+        marque: 22
+      },
+      visiteur: {
+        nom: "Montaigu",
+        marque: 22
+      }
+    }, {
+      id: 3,
+      periode: 1,
+      date: new Date("2016-10-16"),
+      hote: {
+        nom: "NEC",
+        marque: 33
+      },
+      visiteur: {
+        nom: "Coulaine",
+        marque: 33
+      }
+    }
+  ]);
+
+var Rencontres = {
+  connecte: false,
+  connexion: function (url) {
+    // console.log(`Connexion: ${url}`)
+    return RxMongo.connect(url)
+  },
+  deconnexion: function () {
+    return RxMongo.disconnect()
+  },
+  lecture: {
+    liste: function () {
+      // console.log(`Connecté? ${Rencontres.connecte}`)
+      if (Rencontres.connecte) 
+        return new RxCollection("rencontres").find({}).toArray()
+      else 
+        return rencontre$
+    },
+    par: {
+      id: function (idRencontre) {
+        if (Rencontres.connecte) 
+          return new RxCollection("rencontres").find({id: idRencontre}).first()
+        else 
+          return rencontre$.filter(rencontre => {
+            // console.log(`Rencontre: ${JSON.stringify(rencontre)}`)
+            return rencontre.id == idRencontre
+          })
+      }
+    }
+  },
+  ajouterr: function (rencontre) {
+
+    if (Rencontres.connecte) 
+      return RxMongo.collection("rencontres").flatMap(rencontres => RxMongo.insert(rencontres, rencontre))
+  },
+  ajouter: function (rencontreNouvelle) {
+    console.log(`Connexion: ${Rencontres.connecte}`)
+    if (Rencontres.connecte) {
+      return new RxCollection("rencontres")
+        .find({})
+        .toArray()
+        .flatMap(rencontres => {
+          console.info(`rencontres: ${JSON.stringify(rencontres)}`)
+          return rencontres
+        })
+        .map(rencontre => {
+          // console.log(`rencontre id: ${rencontre.id}`)
+          return rencontre.id
+        })
+        .max()
+        .map(idCourant => {
+          console.log(`rencontre idCourant: ${idCourant}`)
+          return idCourant + 1;
+        })
+        .map(idCalcule => {
+          console.log(`rencontre idCalcule: ${idCalcule}`)
+          rencontreNouvelle.id = idCalcule
+          return rencontreNouvelle
+        })
+        .flatMap(rencontreAAjouter => {
+          // console.log(`rencontre: ${JSON.stringify(rencontreAAjouter)}`)
+          return new RxCollection("rencontres")
+            .insert(rencontreAAjouter)
+            .map(result => {
+              // console.info(result)
+              if (!result) 
+                return 0
+              if (result.result.ok != 1) 
+                return 0
+              if (result.insertedCount == 1) 
+                return rencontreAAjouter.id
+            })
+        })
+    }
+  },
+  max: function () {
+    console.log(`Connexion: ${Rencontres.connecte}`)
+    if (Rencontres.connecte) {
+      return new RxCollection("rencontres")
+        .find({})
+        .toArray()
+        .flatMap(rencontres => {
+          console.info(`rencontree id: ${rencontres}`)
+          return rencontres
+        })
+        .map(rencontre => {
+          console.log(`rencontre id: ${rencontre.id}`)
+          return rencontre.id
+        })
+    }
+  },
+  supprimer: {
+    par: {
+      critere: function (critere) {
+        if (Rencontres.connecte) 
+          return new RxCollection("rencontres").deleteOne(critere)
+      }
+    }
   }
 }
+
+module.exports = Rencontres
--- test/Rencontres.spec.js
+++ test/Rencontres.spec.js
@@ -1,28 +1,117 @@
-import chai from "chai"
-import {RxMongo, RxCollection} from 'rxmongo'
-import {MongoClient} from 'mongodb'
-import {Rencontres} from '../src/rencontres'
+var chai = require("chai")
+var {RxMongo, RxCollection} = require('rxmongo')
+var Rencontres = require('../src/Rencontres')
 
-let assert = chai.assert
-let expect = chai.expect
-
-chai.should()
+var expect = chai.expect
+var should = chai.should()
 
 describe("Gestion des rencontres", () => {
-  describe("Etant donné que la BDD est ouverte", () => {
-    let rencontre
-    before(() => {
-      rencontre = new Rencontres("mongodb://@localhost:27017/baskoredb")
-    })
-    it("Lit une rencontre", () => {
-      rencontre
-        .lecture()
-        .first()
+  describe("Etant donné que la BDD n'est pas ouverte", () => {
+    it("on peut lister les rencontres statiques de secours", (done) => {
+      Rencontres
+        .lecture
+        .liste()
         .subscribe(rencontre => {
-          expect(rencontre).to.exist
+          // console.info(rencontre)
+
         }, err => {
           expect(err).to.not.exist
-        })
+        }, () => done())
+    })
+    it("on peut lire une rencontre dans la liste statique de secours", (done) => {
+      Rencontres
+        .lecture
+        .par
+        .id(1)
+        .subscribe(rencontre => {
+          // console.info(rencontre)
+          expect(rencontre).to.exist
+          expect(rencontre.id)
+            .to
+            .equal(1)
+        }, err => {
+          expect(err).to.not.exist
+        }, () => done())
+    })
+  })
+  describe("Etant donné que la BDD est ouverte", () => {
+    before((done) => {
+      // console.info(`Connecté? ${Rencontres.connecte}`)
+      if (!Rencontres.connecte) 
+        Rencontres.connexion("mongodb://@localhost:27017/baskoredb").subscribe(db => {
+          Rencontres.connecte = true
+          // console.info(`Connecté? ${Rencontres.connecte}`)
+        }, err => {
+          console.log(`Err: ${err}`)
+        }, () => done())
+    })
+    after((done) => {
+      // console.info(`Connecté? ${Rencontres.connecte}`)
+      Rencontres.deconnexion()
+      done()
+    })
+    it("on peut lister les rencontres", (done) => {
+      Rencontres
+        .lecture
+        .liste()
+        .subscribe(rencontre => {
+          console.info(rencontre)
+        }, err => {
+          expect(err).to.not.exist
+        }, () => done())
+    })
+    it("on peut lire une rencontre", (done) => {
+      Rencontres
+        .lecture
+        .par
+        .id(3)
+        .subscribe(rencontre => {
+          // console.info(rencontre) expect(rencontre).to.exist
+          expect(rencontre.id)
+            .to
+            .equal(3)
+        }, err => {
+          expect(err).to.not.exist
+        }, () => done())
+    })
+    it("on peut ajouter une rencontre", (done) => {
+      var rencontrer = {
+        periode: 1,
+        date: new Date("2016-09-01"),
+        hote: {
+          nom: "test hote",
+          marque: 11
+        },
+        visiteur: {
+          nom: "test visiteur",
+          marque: 11
+        }
+      };
+      Rencontres
+        .ajouter(rencontrer)
+        .subscribe(idRencontre => {
+          console.info(`Id de la nouvelle rencontre: ${idRencontre}`)
+          expect(idRencontre)
+            .to
+            .not
+            .equal(0);
+        }, err => {
+          expect(err).to.not.exist
+        }, () => done())
+    })
+    it("on peut supprimer une rencontre", (done) => {
+      Rencontres
+        .supprimer
+        .par
+        .critere({id: 4})
+        .subscribe(result => {
+          expect(result).to.exist;
+          expect(result.result.ok)
+            .to
+            .equal(1);
+        }, err => {
+          expect(err).to.not.exist
+        }, () => done())
     })
   })
 })
