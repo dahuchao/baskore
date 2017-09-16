79b9c73cdfda5c6f08bd70f3729aa9d2b92bfda5
dahuchao <dahuchao.github@laposte.net>
2017-09-16 14:52:36 +0200

    configuration

--- src/app.js
+++ src/app.js
@@ -1,17 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import injectTapEventPlugin from 'react-tap-event-plugin'
-injectTapEventPlugin();
 import {Router, IndexRedirect, Route, useRouterHistory} from "react-router"
 import {createHashHistory} from "history"
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider"
 import AccueilConteneur from "./accueil/accueil-conteneur.js"
 import RencontresConteneur from "./rencontres/rencontres-conteneur.js"
 import RencontreConteneur from "./rencontre/rencontre-conteneur.js"
+injectTapEventPlugin();
 
 // const appHistory = useRouterHistory(createHashHistory)({queryKey: false})
 const appHistory = useRouterHistory(createHashHistory)()
-
 
 const App = () => (
   <MuiThemeProvider>
